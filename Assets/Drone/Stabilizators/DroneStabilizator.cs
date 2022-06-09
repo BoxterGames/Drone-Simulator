a7c7a6f36f57e3ad4fac9290558e096d9dc9a47f
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-09 17:24:31 +0500

    unuse

--- Assets/Drone/Stabilizators/DroneStabilizator.cs
+++ Assets/Drone/Stabilizators/DroneStabilizator.cs
@@ -2,15 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public struct MotorInfo
-{
-    public DroneMotor Motor;
-    public float PitchCorrection;
-    public float RollCorrection;
-    public float YawCorrection;
-}
-
 public class DroneStabilizator : MonoBehaviour
 {
     [Header("Motors")]
