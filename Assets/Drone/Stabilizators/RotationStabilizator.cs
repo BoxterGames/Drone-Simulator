648dd928cb83fc8c6d5d900b415ef9f08ee72ddc
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-12 18:13:20 +0500

    weight to rotation stabilizator

--- Assets/Drone/Stabilizators/RotationStabilizator.cs
+++ Assets/Drone/Stabilizators/RotationStabilizator.cs
@@ -14,19 +14,20 @@ public class RotationStabilizator : AbstractStabilizator
     public float BankLimit = 25;
     public float[] MotorCompensation = new float[4];
     public Gyro Gyroscope;
-
+    [Range(0,1)]
+    public float StabllizationPower = 0.5f;
     public override float[] CalculateMotorsPower(DroneControlllerData data)
     {
         float input = CurrentAxis == Axis.Roll ? data.Roll : data.Pitch;
         float value = CurrentAxis == Axis.Roll ? Gyroscope.Roll : Gyroscope.Pitch;
 
-        float correction = PID.Update(input * BankLimit, value, Time.deltaTime);
+        float correction = PID.Update(input * BankLimit - value, Time.deltaTime);
 
         float[] power = new float[MotorCount];
 
         for (int i = 0; i < power.Length; i++)
         {
-            power[i] = Mathf.Clamp(correction * MotorCompensation[i], -1, 1);
+            power[i] = Mathf.Clamp(correction * MotorCompensation[i], -1, 1) * StabllizationPower;
         }
 
         return power;
