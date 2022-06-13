0118572088bf820423f03ac7f0466628e1a4ae48
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-13 13:33:51 +0500

    epmty string

--- Assets/Drone/Stabilizators/AbstractStabilizator.cs
+++ Assets/Drone/Stabilizators/AbstractStabilizator.cs
@@ -7,6 +7,7 @@ public abstract class AbstractStabilizator : MonoBehaviour
     public StabilizationType Type;
     public PID PID;
     public int MotorCount;
+
     public abstract float CalculateMotorsPower(DroneControlllerData data);
 
     public virtual void Reset()
