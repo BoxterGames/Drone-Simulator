4fb3bea9c0d7a101632c7df94d66f50c0cad0b37
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-21 15:57:42 +0500

    Height stabilizate additional due speed

--- Assets/Drone/Stabilizators/HeightStabilizator.cs
+++ Assets/Drone/Stabilizators/HeightStabilizator.cs
@@ -5,10 +5,26 @@ using UnityEngine;
 public class HeightStabilizator : AbstractStabilizator
 {
     public Lidar Lidar;
+    [Range(0,1)]
+    public float AntigravityPower = 0.2f;
+
+    public float VerticalSpeed = 2;
+
+    public PID PidHeight;
+
 
     public override float CalculateMotorsPower(DroneControlllerData data)
     {
-        float heightPower = PID.Update(data.Height - Lidar.Distance, Time.deltaTime);
-        return Mathf.Clamp(heightPower, 0, 1);
+        float deltaHeight = data.Height - Lidar.Distance;
+        float input = PidHeight.Update(deltaHeight, Time.deltaTime);
+
+        input = Mathf.Clamp(input, -1, 1);
+
+        float speed = Lidar.Speed;
+        float idealSpeed = input * VerticalSpeed;
+        float heightPower = PID.Update(idealSpeed - speed, Time.deltaTime);
+     //   Debug.Log(idealSpeed + " " + speed);
+
+        return Mathf.Clamp(heightPower, AntigravityPower, 1);
     }
 }
