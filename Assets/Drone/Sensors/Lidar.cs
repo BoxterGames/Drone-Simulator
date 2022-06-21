bcff6f63ed4d0c9445679b1f6bdb0d2000bdc445
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-06-21 15:58:18 +0500

    exclude zeros

--- Assets/Drone/Sensors/Lidar.cs
+++ Assets/Drone/Sensors/Lidar.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Lidar : MonoBehaviour
 {
     public float Distance { get; private set; }
+    public float Speed { get; private set; }
 
-    private void Update()
+    private float prevDistance;
+
+    private void LateUpdate()
     {
         Physics.Raycast(transform.position, Vector3.down * 100000, out RaycastHit hitInfo);
         Distance = hitInfo.distance;
+
+        if (Distance != prevDistance)
+        {
+            Speed = (Distance - prevDistance) / Time.deltaTime;
+        }
+
+        prevDistance = Distance;
     }
 }
