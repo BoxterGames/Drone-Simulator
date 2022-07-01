38f39e6ba7062729da1569c68750a84d667bf8bf
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-07-01 11:03:34 +0500

    add sensors data

---
+++ Assets/Drone/Sensors/SensorsData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sensors
+{
+    public struct SensorsData
+    {
+        public float Height;
+        public Vector3 EulerAngles;
+        public Vector3 AngularVelocity;
+    }
+}
