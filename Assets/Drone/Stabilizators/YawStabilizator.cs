3ade7eaac218db258d02014753a5e6cf501a1587
roman.karimov@finch-vr.com <roman.karimov@finch-vr.com>
2022-07-06 19:02:44 +0500

    add yaw speed

--- Assets/Drone/Stabilizators/YawStabilizator.cs
+++ Assets/Drone/Stabilizators/YawStabilizator.cs
@@ -7,24 +7,17 @@ namespace Stabilizators
 {
     public class YawStabilizator : AbstractStabilizator
     {
-        public PID Speed;
-        public float MaximumSpeed = 360;
+        public VelocityPID VelocityPID;
 
         public override float CalculateMotorsPower(DroneControlllerData data, SensorsData sensorData, float dt)
         {
-            float delta = AngleNormalizer.NormalizeAngle(data.Yaw - sensorData.EulerAngles.y);
-            float input = Mathf.Clamp(PID.Update(delta, dt), -1, 1);
-
-            float deltaSpeed = MaximumSpeed * input - sensorData.AngularVelocity.y;
-            float motorPower = Mathf.Clamp(Speed.Update(deltaSpeed, dt), -1, 1);
-
-            return motorPower;
+            float p = AngleNormalizer.NormalizeAngle(data.Yaw - sensorData.EulerAngles.y);
+            return VelocityPID.GetValue(p, sensorData.AngularVelocity.y, dt);
         }
 
         public override void Reset()
         {
-            base.Reset();
-            Speed.Reset();
+            VelocityPID.Reset();
         }
     }
 }
